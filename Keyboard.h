fe7f647e316cc01aec689f347d33bbbd3272c525
StevenAvery <Steven.Avery@UOIT.net>
2016-11-10 18:21:00 -0500

    Added keyboard class.

---
+++ Keyboard.h
@@ -0,0 +1,13 @@
+
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+class Keyboard {
+	public:
+		void initKeys();
+		void setKeyDown(unsigned char key);
+		void setKeyUp(unsigned char key);
+		bool isDown(unsigned char key);
+};
+
+#endif
